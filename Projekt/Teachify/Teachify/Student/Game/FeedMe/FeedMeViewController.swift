d2e9b53adea6f245c309e211482ae97ebaf57665
Angelina Scheler <angelina.scheler@hof-university.de>
2018-05-09 20:17:33 +0200

    update FeedMeViewController.swift

--- Projekt/Teachify/Teachify/Student/Game/FeedMe/FeedMeViewController.swift
+++ Projekt/Teachify/Teachify/Student/Game/FeedMe/FeedMeViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+<<<<<<< Updated upstream
 
 class FeedMeViewController : UIViewController {
     
@@ -15,4 +16,76 @@ class FeedMeViewController : UIViewController {
         super.viewDidLoad()
     }
     
+=======
+import GameplayKit
+import SpriteKit
+
+class FeedMeViewController : UIViewController {
+    
+    let skView: SKView = {
+        let view = SKView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+        view.addSubview(skView)
+        
+        skView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        skView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        skView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        skView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        
+        //        if let view = self.view as! SKView? {
+        // Load the SKScene from 'GameScene.sks'
+        if let scene = SKScene(fileNamed: "GameFeedMeScene") {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            skView.presentScene(scene)
+        } else {
+            print("error create FeedMe Scene")
+        }
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        //        }
+        
+        
+           
+            
+        }
+    
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+>>>>>>> Stashed changes
 }
