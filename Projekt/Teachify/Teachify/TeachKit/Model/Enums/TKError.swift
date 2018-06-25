05a9e5b3496f6f8b5949dfde1ce1a797af3c9811
Philipp Knoblauch <>
2018-06-25 10:16:01 +0200

    Added LocalizedDescription to TKErrors

--- Projekt/Teachify/Teachify/TeachKit/Model/Enums/TKError.swift
+++ Projekt/Teachify/Teachify/TeachKit/Model/Enums/TKError.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CloudKit
 
-enum TKError {
+enum TKError: Error {
     case networkUnavailable
+    
     case networkFailure
     case notAuthenticated
     
@@ -37,6 +38,28 @@ enum TKError {
         case CKError.networkFailure.rawValue: self = .networkFailure
         case CKError.notAuthenticated.rawValue: self = .notAuthenticated
         default: return nil
+        }
+    }
+    
+    var localizedDescription: String {
+        switch self {
+        case .networkUnavailable: return "Es besteht keine Internetverbindung"
+        case .networkFailure: return "Es besteht eine Internetverbindung, es konnte allerdings keine Verbindung zur Cloud hergestellt werden."
+        case .wrongRecordZone: return "Die Operation wird auf der falschen RecordZone ausgeführt oder existiert nicht. An Schnittstelle wenden."
+        case .failedSharing: return "Das Objekt konnte nicht geteilt werden."
+        case .parentObjectIsFaulty: return "Operation konnte nicht ausgeführt werden, da das Parent-Objekt Fehlerhaft ist."
+        case .objectIsFaulty: return "Operation konnte nicht ausgeführt werden, da das Objekt Fehlerhaft ist."
+        case .objectIsFaultyAfterCloudUpload: return "Auf dem Objekt wurde eine Operation in der Cloud ausgeführt. Das von der Cloud erhaltene Objekt ist inkonsistent."
+        case .userCouldNotLoad: return "Beim Zugriff auf die Nutzer Informationen ist ein Fehler unterlaufen."
+        case .updateOperationFailed: return "Der Datensatz konnte nicht geupdated werden."
+        case .deleteOperationFailed: return "Der Datensatz konnte nicht gelöscht werden."
+        case .createOperationFailed: return "Der Datensatz konnte nicht erstellt werden."
+        case .addOperationFailed: return "Der Datensatz konnte nicht hinzugefügt werden."
+        case .fetchSortTypeNotAvailable: return "Das Attribut nach dem sortiert werden soll existiert nicht."
+        case .noWritePermission: return "Der Nutzer hat keine Berechtigung die Daten zu ändern."
+        case .noSharedData: return "Die Operation konnte nicht ausgeführt werden, da noch keine Daten geteilt werden."
+        default:
+            return "Unknown Error"
         }
     }
 }
